72ba497dbdb7b78c26c04873c6e24dd727903aba
eirik lysenstoen <elysenstoen@gmail.com>
2020-09-22 18:54:24 +0200

    one quickfix for items and some half-finished UI kit additions

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,5 @@ class Item < ApplicationRecord
   has_one_attached :image
   # the picture the user takes when scanning a clothing item.
   # use item.image.attached? to check for an image
-  validates :photos, presence: true, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..5.megabytes }
+  validates :image, presence: true, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..5.megabytes }
 end
