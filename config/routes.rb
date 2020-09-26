819d84ae61da5c70c33d80562b5db75dac0edbcc
Tomine Odegard <tomineodegard@hotmail.no>
2020-09-26 15:51:06 +0200

    Max and I worked on the flow with connecting right stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items, only: [:new, :show, :index, :destroy, :create, :update, :edit]
-  get '/color', to: 'pages#color'
+  resources :items, only: [:new, :show, :index, :destroy, :create, :update, :edit] do
+    member do
+      get :color
+    end
+  end
 end
