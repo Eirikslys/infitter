5b26795406a3e1e681823eb486a8992e38434b03
Agata Wojcik <agataizabelawojcik@gmail.com>
2020-10-01 20:27:53 +0200

    added fancy submit button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -77,5 +77,31 @@ document.addEventListener("turbolinks:load", () => {
   });
 });
 
+$(document).ready(function () {
+  var timer = null;
+  var self = $("button");
+  var clicked = false;
+  $("button").on("click", function () {
+    if (clicked === false){
+      self.removeClass("filled");
+      self.addClass("circle");
+      self.html("");
+      clicked = true;
+      $("svg.circle-icon").css("display", "block");
+      $(".circle_2").attr("class", "circle_2 fill_circle");
+
+      timer = setInterval(
+        function tick() {
+        self.removeClass("circle");
+        self.addClass("filled");
+        // self.html("b");
+        $(".wrap .checkmark").css("display", "block");
+        $("svg.circle-icon").css("display", "none");
+        clearInterval(timer);
+      }, 2500);
+      }
+  });
+});
+
 
 
