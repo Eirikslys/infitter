abfbe7db63ce8a0afe4e21a521ba1a03ef44e65f
Agata Wojcik <agataizabelawojcik@gmail.com>
2020-10-04 16:43:14 +0200

    I added css to login-page, changed the background color of the alert to transparent

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -83,30 +83,30 @@ document.addEventListener("turbolinks:load", () => {
   });
 });
 
-// $(document).ready(function () {
-//   var timer = null;
-//   var self = $("button");
-//   var clicked = false;
-//   $("button").on("click", function () {
-//     if (clicked === false){
-//       self.removeClass("filled");
-//       self.addClass("circle");
-//       self.html("");
-//       clicked = true;
-//       $("svg.circle-icon").css("display", "block");
-//       $(".circle_2").attr("class", "circle_2 fill_circle");
+$(document).ready(function () {
+  var timer = null;
+  var self = $("button");
+  var clicked = false;
+  $("button").on("click", function () {
+    if (clicked === false){
+      self.removeClass("filled");
+      self.addClass("circle");
+      self.html("");
+      clicked = true;
+      $("svg.circle-icon").css("display", "block");
+      $(".circle_2").attr("class", "circle_2 fill_circle");
 
-//       timer = setInterval(
-//         function tick() {
-//         self.removeClass("circle");
-//         self.addClass("filled");
-//         // self.html("b");
-//         $(".wrap .checkmark").css("display", "block");
-//         $("svg.circle-icon").css("display", "none");
-//         clearInterval(timer);
-//       }, 2500);
-//       }
-//   });
-// });
+      timer = setInterval(
+        function tick() {
+        self.removeClass("circle");
+        self.addClass("filled");
+        // self.html("b");
+        $(".wrap .checkmark").css("display", "block");
+        $("svg.circle-icon").css("display", "none");
+        clearInterval(timer);
+      }, 2500);
+      }
+  });
+});
 
 
