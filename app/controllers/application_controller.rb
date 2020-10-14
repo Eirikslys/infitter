ee5ac308a7e4caa5bfbfa55c9fb2b7f7eb991a41
Tomine Odegard <tomineodegard@hotmail.no>
2020-10-14 14:45:38 +0200

    metatags for the checklist on kit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 end
+
+def default_url_options
+  { host: ENV["www.infitter.net"] || "localhost:3000" }
+end
